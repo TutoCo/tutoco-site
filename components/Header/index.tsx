27072e0a8202ca85e7df72a96be736cca397a3fa
david sy <121736336+davidwsy@users.noreply.github.com>
2024-03-26 11:24:03 -0400

    Update header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -155,17 +155,17 @@ const Header = () => {
             <ThemeToggler />
 
             <Link
-              href="https://github.com/NextJSTemplates/solid-nextjs"
+              href="https://github.com/TutoCo/tutoco-site"
               className="text-regular font-medium text-waterloo hover:text-primary"
             >
               GitHub Repo 🌟
             </Link>
 
             <Link
-              href="https://nextjstemplates.com/templates/solid"
+              href="/auth/sign-up"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
             >
-              Get Pro 🔥
+              Sign Up Now! 🔥
             </Link>
           </div>
         </div>
