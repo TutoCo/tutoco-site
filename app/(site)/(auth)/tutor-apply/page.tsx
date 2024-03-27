349d0c9790a7839d7f743b370fd2424a8228bc28
david sy <121736336+davidwsy@users.noreply.github.com>
2024-03-27 10:37:59 -0400

    Update tutor-apply meta

--- app/(site)/(auth)/tutor-apply/page.tsx
+++ app/(site)/(auth)/tutor-apply/page.tsx
@@ -2,8 +2,8 @@ import Signin from "@/components/Auth/Signin";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login Page - Solid SaaS Boilerplate",
-  description: "This is Login page for Startup Pro",
+  title: "Apply to be a Tutor",
+  description: "Apply to be a tutor for TutoCo!",
   // other metadata
 };
 
