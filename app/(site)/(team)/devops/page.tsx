85db6c7086410b148c6b6cdbd504cba1ccd3f2ed
david sy <121736336+davidwsy@users.noreply.github.com>
2024-03-27 14:48:36 -0400

    point to blogdevops

--- app/(site)/(team)/devops/page.tsx
+++ app/(site)/(team)/devops/page.tsx
@@ -1,5 +1,5 @@
-import BlogData from "@/components/Blog/blogData";
-import BlogItem from "@/components/Blog/BlogItem";
+import BlogData from "@/components/Blog-devops/blogData";
+import BlogItem from "@/components/Blog-devops/BlogItem";
 import SectionHeader from "components/Common/SectionHeader";
 import { Metadata } from "next";
 
