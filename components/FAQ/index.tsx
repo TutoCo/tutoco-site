c03c58142e8b584d3d33447af1373e3816b77020
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-01 13:14:13 -0400

    try nl

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -54,8 +54,10 @@ const FAQ = () => {
                 FAQs FOR TUTEES
               </span>
               <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-                Frequently Asked
-                
+                Frequently Asked<br/>Questions
+{/*                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
+                  Questions
+                </span> */}
               </h2>
 
               {/*<a
