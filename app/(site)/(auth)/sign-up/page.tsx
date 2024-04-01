b1de75f4cc6569e1aebf0844fff50c14c1c85b10
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-01 11:35:06 -0400

    sign up

--- app/(site)/(auth)/sign-up/page.tsx
+++ app/(site)/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import Signup from "@/components/Auth/Signup";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign Up",
+  title: "Tutee Sign-Up",
   description: "Sign up for tutoring!",
   // other metadata
 };
