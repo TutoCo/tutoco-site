75a5cf00fc75262fac00d706e66967147a54d5cd
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-01 13:55:00 -0400

    top20

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -53,7 +53,7 @@ const FAQ = () => {
               <span className="font-medium uppercase text-black dark:text-white">
                 FAQs FOR TUTEES
               </span>
-              <h2 className="sticky top-0 left-0 right-0 mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+              <h2 className="sticky top-20 left-0 mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 Frequently Asked<br/>
                 Questions
               </h2>
