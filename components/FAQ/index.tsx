d4543b8b3ab942c55e9d24fb56b4b4f2101c2ac0
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-01 13:17:48 -0400

    stickya

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -53,8 +53,9 @@ const FAQ = () => {
               <span className="font-medium uppercase text-black dark:text-white">
                 FAQs FOR TUTEES
               </span>
-              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-                Frequently Asked<br/>Questions
+              <h2 className="sticky mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+                Frequently Asked<br/>
+                Questions
 {/*                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
                   Questions
                 </span> */}
