f3a6a43c160d66472e2d55460c02b9760cac719a
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-01 12:44:38 -0400

    sticky7

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -50,17 +50,17 @@ const FAQ = () => {
               viewport={{ once: true }}
               className="animate_left md:w-2/5 lg:w-2/5"
             >
-              <div className="sticky top-0">
-                <span className="font-medium uppercase text-black dark:text-white">
-                  FAQs FOR TUTEES
+
+              <span className="font-medium uppercase text-black dark:text-white">
+                FAQs FOR TUTEES
+              </span>
+              <h2 className="sticky mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+                Frequently Asked
+                <span className="sticky inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
+                  Questions
                 </span>
-                <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
-                  Frequently Asked
-                  <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
-                    Questions
-                  </span>
-                </h2>
-              </div>
+              </h2>
+              
 
               {/*<a
                 href="#"
