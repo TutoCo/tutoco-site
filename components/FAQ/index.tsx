609f6bd7e4bc1d64844c30eae4d2b6d68dd97531
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-01 13:11:41 -0400

    try

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -55,9 +55,7 @@ const FAQ = () => {
               </span>
               <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 Frequently Asked
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
-                  Questions
-                </span>
+                
               </h2>
 
               {/*<a
