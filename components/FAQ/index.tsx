c244eba688429aea19a50b91623c2edd51400272
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-01 13:28:57 -0400

    ticky d

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -48,12 +48,12 @@ const FAQ = () => {
               whileInView="visible"
               transition={{ duration: 1, delay: 0.1 }}
               viewport={{ once: true }}
-              className="sticky top-0 animate_left md:w-2/5 lg:w-2/5"
+              className="relative animate_left md:w-2/5 lg:w-2/5"
             >
               <span className="font-medium uppercase text-black dark:text-white">
                 FAQs FOR TUTEES
               </span>
-              <h2 className="mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+              <h2 className="sticky top-0 mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 Frequently Asked<br/>
                 Questions
 {/*                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
