90daa172fa08f1b8260aa8a46d49a822df8d27bd
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-11 19:31:10 -0400

    dispolay

--- components/Blog-board/index.tsx
+++ components/Blog-board/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import SectionHeader from "../Common/SectionHeader";
 import BlogItem from "./BlogItem";
-import BlogData from "./blogData";
+import * from "./blogData";
+// import { BlogData_exec, BlogData_exec, BlogData_exec, BlogData_exec, BlogData_exec } from "./blogData";
 
 const Blog = async () => {
   return (
@@ -11,8 +12,20 @@ const Blog = async () => {
 
       <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
         <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
-          {BlogData.slice(0, 3).map((blog, key) => (
+          {BlogData_exec.slice(0, 3).map((blog, key) => (
             <BlogItem blog={blog} key={key} />
+          ))}
+          {BlogData.slice(0, 3).map((blog, key) => (
+            <BlogItem_mark blog={blog} key={key} />
+          ))}
+          {BlogData.slice(0, 3).map((blog, key) => (
+            <BlogItem_tuto blog={blog} key={key} />
+          ))}
+          {BlogData.slice(0, 3).map((blog, key) => (
+            <BlogItem_comm blog={blog} key={key} />
+          ))}
+          {BlogData.slice(0, 3).map((blog, key) => (
+            <BlogItem_logi blog={blog} key={key} />
           ))}
         </div>
       </div>
