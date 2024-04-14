0f2cc15268551cd36b78d6cc1ed73fe35846a0ac
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-13 21:28:19 -0400

    mxuato

--- components/Blog-board/BlogItem.tsx
+++ components/Blog-board/BlogItem.tsx
@@ -28,9 +28,9 @@ const BlogItem = ({ blog }: { blog: Blog }) => {
         className="animate_top rounded-lg bg-white p-4 pb-9 shadow-solid-8 dark:bg-blacksection"
       >
 {/*         <Link href={`/blog/`} className="relative block aspect-[368/239]">
-          <Image src={mainImage} alt={title} fill />
+          <Image src={mainImage} alt={title} fill /> object-contain  items-center
         </Link> */}
-        <Link href={`/blog/`} className="relative block">
+        <Link href={`/blog/`} className="relative block mx-auto">
           <Image src={mainImage} alt={title} />
         </Link>
 
