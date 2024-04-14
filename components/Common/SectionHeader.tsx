ff901fdad23db99606468cd72e68ea5daf90469e
david sy <121736336+davidwsy@users.noreply.github.com>
2024-04-14 17:02:54 +0000

    dangerhtml here

--- components/Common/SectionHeader.tsx
+++ components/Common/SectionHeader.tsx
@@ -39,7 +39,11 @@ const SectionHeader = ({ headerInfo }: { headerInfo: HeaderInfo }) => {
         <h2 className="mx-auto mb-4 text-3xl font-bold text-black dark:text-white md:w-4/5 xl:w-1/2 xl:text-sectiontitle3">
           {subtitle}
         </h2>
-        <p className="mx-auto md:w-4/5 lg:w-3/5 xl:w-[60%]">{description}</p>
+{/*         <p className="mx-auto md:w-4/5 lg:w-3/5 xl:w-[60%]">{description}</p> */}
+        <p
+          className={`mx-auto md:w-4/5 lg:w-3/5 xl:w-[60%]`}
+          dangerouslySetInnerHTML={{__html:description}}>
+        </p>
       </motion.div>
       {/* <!-- Section Title End --> */}
     </>
